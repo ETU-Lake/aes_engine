e4867766e6b15a93291cf4b57278b3cddfb20754
Aydin Mercan <aydin@mercan.dev>
2020-07-15 18:19:07 +0300

    faster keyexpansion (51 clock cycle)

--- sim_1/new/test_keyexpansion.v
+++ sim_1/new/test_keyexpansion.v
@@ -17,12 +17,11 @@ module test_keyexpansion();
 
     initial begin
         clk = 0; rst = 0; start = 0;
-        //in = {8'h6b, 8'h20, 8'h65, 8'h64, 8'h79, 8'h62, 8'h2d, 8'h32,
-          //  8'h33, 8'h20, 8'h64, 8'h6e, 8'h61, 8'h70, 8'h78, 8'h65 };
         in = {8'h65, 8'h78, 8'h70, 8'h61, 8'h6E, 8'h64, 8'h20, 8'h33, 
               8'h32, 8'h2D, 8'h62, 8'h79, 8'h74, 8'h65, 8'h20, 8'h6B };
         #2;
         start = 1;
+        /*
         #2;
         start = 0;
         #20;
@@ -31,6 +30,7 @@ module test_keyexpansion();
         rst = 0;
         #2;
         start = 1;
+        */
     end
 
     always begin
--- sources_1/new/keyexpansion.v
+++ sources_1/new/keyexpansion.v
@@ -1,5 +1,7 @@
 `timescale 1ns / 1ps
 
+// TODO: better rcon handling
+
 module keyexpansion(
     input [127:0] key,
     input start,
@@ -10,7 +12,7 @@ module keyexpansion(
 );
     reg [10:0] i, j, k;
     reg [7:0] tmp[3:0], sboxin[3:0], rcon;
-    reg toggle;
+    reg run;
 
     wire [7:0] sboxout[3:0];
 
@@ -20,23 +22,29 @@ module keyexpansion(
     sbox tmp4 (.in(sboxin[3]), .out(sboxout[3]));
 
     initial begin
+        run <= 1'b0;
         finish <= 1'b0;
         i <= 10'd4;
         rcon <= 8'd0;
         out = 1048'd0;
     end
 
-    always @ (posedge start) begin
-        out[1407-:128] = key;
-    end
+    /* 
+     * This can run in both posedge and negedge. Heavy sbox operations are
+     * seperated across 2 cycles.
+     */
+    always @ (clk) begin
+        if (rst) begin
+            run <= 1'b0;
+            i <= 10'd4;
+            rcon <= 8'd0;
+        end else if (start && ~run) begin
+            out = 1048'd0;
+            out[1407-:128] = key;
+            run = ~run;
+        end
 
-    always @ (posedge rst) begin
-        i = 10'd0;
-        finish <= 1'b0;
-        out <= 1048'd0;
-    end
-
-    always @ (posedge i) begin
+        if (run) begin
             if (i < 44) begin
                 k = (i-1)*4;
 
@@ -46,65 +54,52 @@ module keyexpansion(
                 tmp[3] = out[1407-(8*(k+3))-:8];
 
                 if (i%4 == 0) begin
-                    sboxin[0] = tmp[1];
-                    sboxin[1] = tmp[2];
-                    sboxin[2] = tmp[3];
-                    sboxin[3] = tmp[0];
+                    if (rcon == 0) begin
+                        sboxin[0] = tmp[1];
+                        sboxin[1] = tmp[2];
+                        sboxin[2] = tmp[3];
+                        sboxin[3] = tmp[0];
+                        case (i/4)
+                            8'd0: rcon = 8'h8D;
+                            8'd1: rcon = 8'h01;
+                            8'd2: rcon = 8'h02;
+                            8'd3: rcon = 8'h04;
+                            8'd4: rcon = 8'h08;
+                            8'd5: rcon = 8'h10;
+                            8'd6: rcon = 8'h20;
+                            8'd7: rcon = 8'h40;
+                            8'd8: rcon = 8'h80;
+                            8'd9: rcon = 8'h1B;
+                            8'd10: rcon = 8'h36;
+                        endcase
+                    end else begin
+                        tmp[0] = sboxout[0];
+                        tmp[1] = sboxout[1];
+                        tmp[2] = sboxout[2];
+                        tmp[3] = sboxout[3];
+                        tmp[0] = tmp[0] ^ rcon;
 
-                    case (i/4)
-                        8'd0: rcon = 8'h8D;
-                        8'd1: rcon = 8'h01;
-                        8'd2: rcon = 8'h02;
-                        8'd3: rcon = 8'h04;
-                        8'd4: rcon = 8'h08;
-                        8'd5: rcon = 8'h10;
-                        8'd6: rcon = 8'h20;
-                        8'd7: rcon = 8'h40;
-                        8'd8: rcon = 8'h80;
-                        8'd9: rcon = 8'h1B;
-                        8'd10: rcon = 8'h36;
-                    endcase
+                        rcon = 0;
+                    end
+                end
+
+                if (rcon == 0) begin
+                    j = i*4;
+                    k = (i-4)*4;
+                    out[1407-8*j-:8]       = out[1407-8*k-:8] ^ tmp[0];
+                    out[1407-(8*(j+1))-:8] = out[1407-(8*(k+1))-:8] ^ tmp[1];
+                    out[1407-(8*(j+2))-:8] = out[1407-(8*(k+2))-:8] ^ tmp[2];
+                    out[1407-(8*(j+3))-:8] = out[1407-(8*(k+3))-:8] ^ tmp[3];
+                    i = i+1;
                 end
 
             end else begin
                 finish = 1'b1;
             end
+        end
     end
 
     always @ (posedge finish) begin
-                $display("%h", out);
+        $display("%h", out);
     end
-
-    always @ (negedge i) begin
-        if (start == 1) begin
-            if (i%4 == 0) begin
-                tmp[0] = sboxout[0];
-                tmp[1] = sboxout[1];
-                tmp[2] = sboxout[2];
-                tmp[3] = sboxout[3];
-                tmp[0] = tmp[0] ^ rcon;
-            end
-
-            j = i*4;
-            k = (i-4)*4;
-
-            out[1407-8*j-:8]       = out[1407-8*k-:8] ^ tmp[0];
-            out[1407-(8*(j+1))-:8] = out[1407-(8*(k+1))-:8] ^ tmp[1];
-            out[1407-(8*(j+2))-:8] = out[1407-(8*(k+2))-:8] ^ tmp[2];
-            out[1407-(8*(j+3))-:8] = out[1407-(8*(k+3))-:8] ^ tmp[3];
-        end
-    end
-
-    always @ (posedge clk) begin
-        if (start) begin
-            i = 10'd4;
-        end
-
-        if (i != 10'd0 && i != 10'd44) begin
-            i = i+10'd1;
-        end else if (i == 10'd44) begin
-            finish = 1'b1;
-        end
-    end
-
 endmodule
