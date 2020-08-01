60d179a2dc5751b784d812d78b3a0dc660b4865a
nuriyesatici <50884142+nuriyesatici@users.noreply.github.com>
2020-08-01 14:08:09 +0300

    Update subbytes.v

--- subbytes.v
+++ subbytes.v
@@ -1,39 +1,36 @@
 `timescale 1ns / 1ps
 
-//Mesaj sinyali 8'er bit olarak ayrýlmýþ olup her 8 biti s box'a input olarak verilip
-//sbox da transformasyona uðrayacaktýr. 
-//Ve sbox dan gelen output da subbyte'ýn outputunu oluþturacaktýr. 
-//Yani mesaj sinyalindeki deðerler sbox'daki deðerler ile yer deðiþtirilmiþ olacaktýr.
+//Mesaj sinyalini 8'er bit olarak ayýrýp her 8 bitini s box'a input olarak verilip
+//sbox da linear transformasyona uðrayacak. 
+//Ve sbox'dan gelen output da subbyte ýn outputu olacaktýr. 
+//Yani mesaj sinyalindeki deðerler sbox'dakilerle yer deðiþtirilmiþ olacaktýr.
 
 module subbytes(
-input [127:0] blok, 
-output [127:0] sb 
+input [127:0] state, 
+output [127:0] out  //Bu aþamadan sonra çýktý ShiftRows'a gidecektir
     );
-    
    // 8 bit yani 1 byte yer deðiþtiriliyor
-     sbox s0( .in(blok[127:120]),.out(sb[127:120]) );
-     sbox s1( .in(blok[119:112]),.out(sb[119:112]) );
-     sbox s2( .in(blok[111:104]),.out(sb[111:104]) );
-     sbox s3( .in(blok[103:96]),.out(sb[103:96]) );
+     sbox s0( .in(state[127:120]),.out(out[127:120]) );
+     sbox s1( .in(state[119:112]),.out(out[119:112]) );
+     sbox s2( .in(state[111:104]),.out(out[111:104]) );
+     sbox s3( .in(state[103:96]),.out(out[103:96]) );
      
-     sbox s4( .in(blok[95:88]),.out(sb[95:88]) );
-     sbox s5( .in(blok[87:80]),.out(sb[87:80]) );
-     sbox s6( .in(blok[79:72]),.out(sb[79:72]) );
-     sbox s7( .in(blok[71:64]),.out(sb[71:64]) );
+     sbox s4( .in(state[95:88]),.out(out[95:88]) );
+     sbox s5( .in(state[87:80]),.out(out[87:80]) );
+     sbox s6( .in(state[79:72]),.out(out[79:72]) );
+     sbox s7( .in(state[71:64]),.out(out[71:64]) );
      
-     sbox s8( .in(blok[63:56]),.out(sb[63:56]) );
-     sbox s9( .in(blok[55:48]),.out(sb[55:48]) );
-     sbox s10(.in(blok[47:40]),.out(sb[47:40]) );
-     sbox s11(.in(blok[39:32]),.out(sb[39:32]) );
+     sbox s8( .in(state[63:56]),.out(out[63:56]) );
+     sbox s9( .in(state[55:48]),.out(out[55:48]) );
+     sbox s10(.in(state[47:40]),.out(out[47:40]) );
+     sbox s11(.in(state[39:32]),.out(out[39:32]) );
      
-     sbox s12(.in(blok[31:24]),.out(sb[31:24]) );
-     sbox s13(.in(blok[23:16]),.out(sb[23:16]) );
-     sbox s14(.in(blok[15:8]),.out(sb[15:8]) );
-     sbox s16(.in(blok[7:0]),.out(sb[7:0]) );
+     sbox s12(.in(state[31:24]),.out(out[31:24]) );
+     sbox s13(.in(state[23:16]),.out(out[23:16]) );
+     sbox s14(.in(state[15:8]),.out(out[15:8]) );
+     sbox s16(.in(state[7:0]),.out(out[7:0]) );
     
 
 endmodule
-
-
 
 
