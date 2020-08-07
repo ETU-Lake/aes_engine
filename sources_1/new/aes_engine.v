6aee0e83b4bf7cb52f54bba38dc150ef02a8e2e3
Serdar Sayın <ssayin@etu.edu.tr>
2020-08-07 12:06:47 +0300

    Add aes_wrapper

--- sources_1/new/aes_engine.v
+++ sources_1/new/aes_engine.v
@@ -1,38 +1,21 @@
 `timescale 1ns / 1ps
 
 module aes_engine(
-    input clk,
-    input rst,
-    input anahtar[127:0],
-    input blok[127:0],
-    input g_gecerli,
-    output hazir,
-    output reg sifre[127:0],
-    output c_gecerli
-);
-
-    reg [1407:0] key;
-
-    initial begin
-        run <= 1'b0;
-        rnd_num <= 4'd0;
-        c_gecerli <= 1'b0;
-        sifre <= 128'd0;
-    end
-
-    always @ (posedge clk) begin
-        if (rst) begin
-           run <= 1'b0;
-           rnd_num <= 4'd0;
-           c_gecerli <= 1'b0;
-           sifre <= 128'd0;
-           hazir <= 1'b0;
-        end else if (g_gecerli && ~run) begin
-            run = ~run;
-        end
-
-        if (run) begin
-        end
-    end
+  input         clk,
+  input         rst,
+  
+  input[127:0]  anahtar,
+  input[127:0]  blok,
+  
+  input         g_gecerli,
+  output        hazir,
+  
+  output[127:0] sifre,
+  output        c_gecerli
+  );
+ 
+  assign hazir = 1'b1;
+  assign sifre = blok ^ anahtar;
+  assign c_gecerli = g_gecerli;
 
 endmodule
