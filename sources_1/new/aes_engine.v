976693f9edad1a1af01ed3d9671b94989276bff7
Aydin Mercan <aydin@mercan.dev>
2020-08-09 21:16:51 +0300

    pipeline fundamental

--- sources_1/new/aes_engine.v
+++ sources_1/new/aes_engine.v
@@ -15,11 +15,11 @@ module aes_engine (
     wire [127:0] states [0:10];
     wire key_expanded;
     reg [127:0] first;
-    reg [3:0] ctr;
+    reg [3:0] ctr [1:0];
     reg run, valid;
 
     assign states[0] = first;
-    assign hazir = key_expanded & (ctr == 4'd0);
+    assign hazir = key_expanded & ((ctr[0] == 4'd0) | (ctr[1] == 4'd0));
     assign sifre = states[10];
     assign c_gecerli = valid;
 
@@ -36,39 +36,55 @@ module aes_engine (
     lastround do_lastround(.clk(clk & run), .rst(rst), .key(expanded[127:0]), .state(states[9]), .out(states[10]));
 
     initial begin
-        run = 1'b0;
-        valid = 1'b0;
-        ctr = 4'd0;
+        run <= 1'b0;
+        valid <= 1'b0;
+        ctr[0] <= 4'd0;
+        ctr[1] <= 4'd0;
     end
 
     always @ (posedge clk) begin
         if (rst) begin
             run <= 1'b0;
-            ctr <= 4'd0;
+            ctr[0] <= 4'd0;
+            ctr[1] <= 4'd0;
             first <= 128'd0;
         end else if (key_expanded && g_gecerli) begin
             run <= 1'b1;
         end
 
         if (valid) begin
-            valid = 1'b0;
+            $display("%d %d", ctr[0], ctr[1]);
+            if (~(ctr[0] == 4'd10 || ctr[1] == 4'd10)) begin
+                valid = ~valid;
+            end
         end
 
         if (g_gecerli && hazir) begin
-            if (ctr == 4'd0) begin
+            if (ctr[0] == 4'd0) begin
                 first = blok ^ expanded[1407-:128];
-                ctr <= 4'd1;
+                ctr[0] <= 4'd1;
             end
+
+            if (ctr[1] == 4'd0) begin
+                first = blok ^ expanded[1407-:128];
+                ctr[1] <= 4'd1;
+            end
+
         end
 
         if (run) begin
-            $display("%d" , ctr);
-            if (ctr == 4'd10) begin
+            if (ctr[0] == 4'd10) begin
                 valid <= 1'b1;
-                ctr <= 4'd0;
-            end else if (~(ctr == 4'd0)) begin
-                ctr = ctr + 4'd1;
+                ctr[0] <= 4'd0;
+            end else if (~(ctr[0] == 4'd0)) begin
+                ctr[0] = ctr[0] + 4'd1;
+            end else if (ctr[1] == 4'd10) begin
+                valid <= 1'b1;
+                ctr[1] <= 4'd0;
+            end else if (~(ctr[1] == 4'd0)) begin
+                ctr[1] = ctr[1] + 4'd1;
             end
+
         end
     end
 endmodule
