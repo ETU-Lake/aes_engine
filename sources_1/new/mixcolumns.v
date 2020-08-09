486c36f1c218338f9161e1cfc031183f0967c671
neptunel <63646687+neptunel@users.noreply.github.com>
2020-08-09 21:23:03 +0300

    Update mixcolumns.v

--- sources_1/new/mixcolumns.v
+++ sources_1/new/mixcolumns.v
@@ -5,33 +5,32 @@ module mixcolumns(
     input clk,
     output reg[127:0] out
 );
-    genvar i;
+   //enc_row her biri 32 bitten olusan 4 bitlik veri tutar.
+  // MixColumns adimindaki matrisin her bir sutununu 4 bitlerin her biri temsil eder.  
     reg[31:0] enc_row[3:0];
-
-    //always @ (posedge clk) begin
-        //Her yeni saat sinyalinde enc_rowdaki degerleri kullanarak output olusturulur.
-        // out = {enc_row[3], enc_row[2], enc_row[1], enc_row[0]};
-    //end
-
+    //gf_mult2 GaloisField(256)'da 2 ile Rijndael matrix carpimi yapan fonksiyondur.
     function [7 : 0] gf_mult2(input [7 : 0] c); begin
             gf_mult2 = {c[6 : 0], 1'b0} ^ (8'h1b & {8{c[7]}});
         end
     endfunction
-
+ //gf_mult3 GaloisField(256)'da 3 ile Rijndael matrix carpimi yapan fonksiyondur.
     function [7 : 0] gf_mult3(input [7 : 0] c); begin
             gf_mult3 = gf_mult2(c) ^ c;
         end
     endfunction
 
     always @ (state) begin: tmp_mixCol
-      reg [7:0] b0, b1, b2, b3, tmp, a0, a1, a2, a3, c0, c1, c2, c3, d0, d1, d2, d3;
+       //asagidaki 8 bit uzunlugundaki reglerin her biri farkli harf(a,b,c,d) olmak uzere 4*4luk matristeki bir sutunu gosterir.
+      reg[7:0] a0,a1,a2,a3,b0, b1, b2, b3,c0,c1,c2,c3,d0,d1,d2,d3;
+      //w herbiri 32 bitlik 4 sutundan olusan matrix gibi dusunulebilir. Yukarida tanimlanan enc_row un yerel degiskeni gibidir.
       reg [31:0] w [3:0];
+      //w nin her bir sutununa inputun sirasiyla 32ser bitleri eklendi.
       w[0] = state[31:0];
       w[1] = state[63:32];
       w[2] = state[95:64];
       w[3] = state[127:96];
 
-
+      //asagidaki farkli harflerin her bir rakam kombinasyonu(0,1,2,3) her bir sutundaki sirasiyla dizilmis 8 bitlik 4 elemani temsil eder.
       a0 = w[0][31:24];
       a1 = w[0][23 : 16];
       a2 = w[0][15 : 8];
@@ -51,6 +50,14 @@ module mixcolumns(
       d1 = w[3][23 : 16];
       d2 = w[3][15 : 8];
       d3 = w[3][7:0];
+       
+     //her bir sutun elemani sirasiyla
+      //  _       _
+      // | 2 3 1 1 |
+      // | 1 2 3 1 |
+      // | 1 1 2 3 |   Rijndael Galois Filed matrix ile carpilir.2 ve 3 ile carpim polinomal olarak 2 yi x gibi 3 u de x+1 gibi kabul ederk isler.
+      // | 3 1 1 2 |
+      // -         -
 
       enc_row[0][31:24] = gf_mult2(a0)  ^ gf_mult3(a1)^ a2 ^ a3;
       enc_row[0][23 : 16] = a0  ^ gf_mult2(a1)^ gf_mult3(a2) ^ a3;
@@ -72,7 +79,8 @@ module mixcolumns(
       enc_row[3][23 : 16] = d0  ^ gf_mult2(d1)^ gf_mult3(d2) ^ d3;
       enc_row[3][15 : 8] = d0  ^ d1^ gf_mult2(d2) ^ gf_mult3(d3);
       enc_row[3][7:0] = gf_mult3(d0)  ^ d1^ d2 ^ gf_mult2(d3);
-
+        
+      //Her yeni yukselen saat darbesinde enc_rowdaki degerleri kullanarak output olusturulur.
       out = {enc_row[3], enc_row[2], enc_row[1], enc_row[0]};
     end
 endmodule
